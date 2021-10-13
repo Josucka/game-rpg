17b6fddbe004d7f089829150d9fd5abd4f912b37
Josué Barros <frajolla27@gmail.com>
2021-10-13 18:00:10 -0300

    <feat>(create)
    rpg game with image manipulation

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { useEffect } from 'react';
+import * as C from './App.styles';
+import { Character } from './components/Character';
+import { useCharacter } from './hooks/useCharacter';
+
+const App = () => {
+  const char = useCharacter();
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch(e.code){
+      case 'keyA':
+      case 'ArrowLeft':
+        char.moveLeft();
+        break;
+      
+      case 'keyW':
+      case 'ArrowUp':
+        char.moveUp();
+        break;
+      
+      case 'keyD':
+      case 'ArrowRight':
+        char.moveRight();
+        break;
+      
+      case 'keyS':
+      case 'ArrowDown':
+        char.moveDown();
+        break;
+    }
+  }
+
+  return (
+    <C.Container>
+      <C.Map>
+        <Character 
+          x={char.x} 
+          y={char.y} 
+          side={char.side} 
+        />
+      </C.Map>
+    </C.Container>
+
+  );
+}
+
+export default App;
